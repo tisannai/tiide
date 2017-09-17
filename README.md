e27c9192297f6df88516149f32e88ee6fdbe1222
Tero <tero.isannainen@gmail.com>
2017-09-17 16:47:37 +0300

    Initial.

---
+++ README.md
@@ -0,0 +1,72 @@
+# Tiide
+
+Tiide is a minimalistic C language IDE for Emacs. It integrates
+c-eldoc, compile and gud-gdb packages to users C project development.
+
+Tiide enables the user to launch compilation and debugging from the
+current buffer. Current buffer identifies the Tiide Project
+indirectly. Tiide finds the Project by traveling up in directory
+hierarchy from the file of current buffer.
+
+Project Root directory is identified by placing ".tiide.el" file to
+the root directory. ".tiide.el" is called Tiide Config. Tiide Project
+covers all files below the Project Root.
+
+Tiide provides following commands:
+
+* tiide-refresh: update Tiide Config cache after editing ".tiide.el".
+
+* tiide-debug: run gud-gdb with Project's ".gdbinit" file.
+
+* tiide-edit: open Project's ".gdbinit" for editing.
+
+* tiide-build: build Project.
+
+
+User is advised to map these commands directly to keys. For example:
+
+    (global-set-key (kbd "C-x 4 u") 'tiide-build)
+    (global-set-key (kbd "C-x 4 g") 'tiide-debug)
+    (global-set-key (kbd "C-x 4 e") 'tiide-edit)
+    (global-set-key (kbd "C-x 4 i") 'tiide-get-breakpoint)
+
+Tiide Config includes information about the users dev env.
+
+Example configuration (".tiide.el content"):
+
+    '(
+        (tiide-compdir . ".")
+        (tiide-compile . "rake test:all")
+        (tiide-gdbinit . ".gdbinit")
+        (tiide-include . ("src" "test" "vendor/ceedling/vendor/unity/src"))
+        )
+
+This configuration is for a project that uses Ceedling for
+testing. "src" include source files and tests are in "test" directory.
+
+The configuration specifies:
+
+* compilation is performed at Root directory, and
+
+* compilation is performed with "rake test:all" command,
+
+* ".gdbinit" file is in the Root directory,
+
+* project include files are in "<root>/src", "<root>/test", and
+  "<root>/vendor/ceedling/vendor/unity/src" directories.
+
+"tiide-compdir" and "tiide-compile" are used by "compile"
+command. "tiide-gdbinit" is used by "gud-gdb".
+
+"tiide-include" is used by "c-eldoc" indirectly. Tiide updates
+"c-eldoc-includes" variable before the function prototype lookup is
+performed. User should setup the base includes to
+"tiide-c-eldoc-include-base" variable, for example in ".emacs":
+
+    (setq tiide-c-eldoc-include-base "`pkg-config gtk+-2.0 --cflags` -I./ -I../")
+
+The base is extended by Project specific "-I" entries. This ensures
+that all function prototypes are found by "c-eldoc".
+
+
+Tiide library by Tero Isannainen, (c) Copyright 2017.
---
+++ tiide.el
@@ -0,0 +1,178 @@
+;;; tiide.el -- TI Integrated Design Environment.
+
+;; Copyright (C) 2017 Tero Isannainen
+
+;; Tiide package includes features that makes the use of gud-gdb and
+;; c-eldoc convenient.
+;;
+;; Tiide env configuration is read from ".tiide.el". Configuration is
+;; called Tiide Config. ".tiide.el" file is placed to the root dir of
+;; project and it is called Tiide Root. The file defines relative
+;; locations for relevant files and also the command used to compile
+;; the project.
+;;
+;; Example:
+;;    '(
+;;        (tiide-gdbinit . ".gdbinit")
+;;        (tiide-compile . "rake test:all")
+;;        (tiide-compdir . ".")
+;;        (tiide-include . ("src" "test" "vendor/ceedling/vendor/unity/src"))
+;;        )
+;;
+;; "tiide-gdbinit" specifies the relative path from Tiide Root to
+;; ".gdbinit" file.
+;;
+;; "tiide-compile" specifies the project's compilation command.
+;;
+;; "tiide-compdir" specifies the project's compilation directory.
+;;
+;; "tiide-include" specifies relative paths that should be part of
+;; "c-eldoc" include paths. These are required in order to show
+;; function signatures in minibuffer when cursor is at function call
+;; in source file buffer.
+;;
+;; User has the following commands available:
+;;   tiide-refresh  - Update Tiide Config (after ".tiide.el" edit).
+;;   tiide-build    - Build project.
+;;   tiide-debug    - Start GDB within Emacs.
+;;   tiide-edit     - Edit project's .gdbinit file in Emacs.
+;;
+;; Suggested global key bindings:
+;;   (global-set-key (kbd "C-x 4 u") 'tiide-build)
+;;   (global-set-key (kbd "C-x 4 g") 'tiide-debug)
+;;   (global-set-key (kbd "C-x 4 e") 'tiide-edit)
+;;   (global-set-key (kbd "C-x 4 i") 'tiide-get-breakpoint)
+
+
+;; ------------------------------------------------------------
+;; User configuration:
+
+(require 'c-eldoc)
+
+(defvar tiide-c-eldoc-include-base
+   "`pkg-config gtk+-2.0 --cflags` -I./ -I../"
+   "Default list of include dirs for c-eldoc. This list is
+   extended with \"tiide-include\" from .tiide.el.")
+
+
+;; ------------------------------------------------------------
+;; Tiide internal implementation:
+
+(defun tiide-find-root ()
+   "Find and return .tiide.el file from dirs above, or nil if not found."
+   (let ((curdir (expand-file-name ".")) (tiide-conf nil))
+      (catch 'exit
+         (while (not (string= "/" curdir))
+            (let ((files (directory-files curdir)))
+               (while (car files)
+                  (if (string= (car files) ".tiide.el")
+                     (progn
+                        (setq tiide-conf (car files))
+                        (throw 'exit "dummy")))
+                  (setq files (cdr files))))
+            (setq curdir (directory-file-name (file-name-directory curdir)))))
+      (if (string= curdir "/")
+         nil
+         curdir)))
+
+
+(defun tiide-get-config-from-file ()
+   "Get all information related to Tiide env."
+   (let ((root (tiide-find-root))
+           (config))
+      (if root
+         (progn
+            (setq config
+               (eval (car
+                        (read-from-string
+                           (with-temp-buffer
+                              (insert-file-contents (format "%s/.tiide.el" root))
+                              (buffer-string))))))
+            (append config
+               (list (cons 'tiide-root root))
+               (list (cons 'tiide-gdbinit-file (format "%s/%s" root (cdr (assoc 'tiide-gdbinit config)))))
+               (list (cons 'tiide-compile-dir (format "%s/%s" root (cdr (assoc 'tiide-compdir config)))))
+               (list (cons 'tiide-gdbinit-dir (directory-file-name
+                                                 (file-name-directory
+                                                    (format "%s/%s" root (cdr (assoc 'tiide-gdbinit config)))))))
+               (list (cons 'tiide-c-eldoc-include
+                        (concat tiide-c-eldoc-include-base " "
+                           (mapconcat 'identity
+                              (mapcar
+                                 (lambda (i)
+                                    (format "-I%s/%s" root i))
+                                 (cdr (assoc 'tiide-include config))) " "))))))
+         nil)))
+
+
+(defun tiide-get-config ()
+   "Get Tiide env configuration either from cache or refresh."
+   (unless (local-variable-p 'tiide-config)
+      (let ((config nil))
+         (setq config (tiide-get-config-from-file))
+         (if config
+            (progn
+               (make-local-variable 'tiide-config)
+               (setq tiide-config config)))))
+   (if (local-variable-p 'tiide-config)
+      tiide-config
+      nil))
+
+(defun tiide-update-c-eldoc-includes ()
+   "Update c-eldoc-includes variable with buffer relevant content."
+   (let ((config (tiide-get-config)))
+      (if config
+         (setq c-eldoc-includes (cdr (assoc 'tiide-c-eldoc-include config))))))
+
+(add-hook 'c-eldoc-get-buffer-hook 'tiide-update-c-eldoc-includes)
+            
+
+
+;; ------------------------------------------------------------
+;; User functions:
+
+(defun tiide-refresh ()
+   "Update Tiide Config after config file edit."
+   (interactive)
+   (let ((config nil))
+      (setq config (tiide-get-config-from-file))
+      (if config
+         (progn
+            (unless (local-variable-p 'tiide-config)
+               (make-local-variable 'tiide-config))
+            (setq tiide-config config)))))
+
+(defun tiide-debug ()
+   "Start GDB using .gdbinit specified in Tiide config."
+   (interactive)
+   (let ((config (tiide-get-config)))
+      (if config
+         (gud-gdb (format "gdb --fullname -nx -x %s" (cdr (assoc 'tiide-gdbinit-file config)))))))
+
+
+(defun tiide-edit ()
+   "Edit .gdbinit file specified in Tiide config."
+   (interactive)
+   (let ((config (tiide-get-config)))
+      (if config
+         (find-file (cdr (assoc 'tiide-gdbinit-file config))))))
+
+
+(defun tiide-build ()
+   "Build/compile based on Tiide config."
+   (interactive)
+   (let ((config (tiide-get-config)))
+      (if config
+         (with-temp-buffer
+            (cd (cdr (assoc 'tiide-compile-dir config)))
+            (compile (cdr (assoc 'tiide-compile config)))))))
+
+(defun tiide-get-breakpoint ()
+   "Storage current buffer and line info to yank buffer."
+   (interactive)
+   (let ((name (buffer-name (current-buffer)))
+           (line (line-number-at-pos)))
+      (kill-new (format "%s:%d" name line))))
+
+
+(provide 'tiide)
